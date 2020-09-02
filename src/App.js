98eb3e59383041c37ece691c67eae654940be333
Mauricio Alejandro <mamv.2137@gmail.com>
2020-09-01 23:43:10 -0300

    Add some components and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Desktop from "Pages/Desktop";
+import Login from "Pages/Login";
 
 function App() {
   return (
     <Router>
+      <Route path="/login" component={Login} />
       <Route path="/" component={Desktop} />
     </Router>
   );
--- src/Pages/Desktop/Desktop.jsx
+++ src/Pages/Desktop/Desktop.jsx
@@ -1,26 +1,10 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
-
-import Icon from "Components/Icon/Icon";
-
-import { cartItemsVar } from "apolloProvider.js";
-
 import "App.css";
 
-const GET_CART_ITEMS = gql`
-  query GetCartItems {
-    cartItems @client
-  }
-`;
-
 const Desktop = () => {
-  const { loading, error, data } = useQuery(GET_CART_ITEMS);
-
-  if (!loading) console.log(data);
-
   return (
     <div className="App-header">
-      <Icon />
+      Desktop...
     </div>
   );
 };
