010ef6a04526198092292bc61e8448ee0d1420f1
Giildev <Giildev.exe@gmail.com>
2020-09-01 20:40:52 -0500

    boilerplate NEW

---
+++ src/Containers/Desktop/NEW/index.jsx
@@ -0,0 +1,17 @@
+// Dependencies
+import React, { Component } from "react";
+
+// Components
+
+// Styles
+import { NEWSTYLE } from "./styles";
+
+export default class NEW extends Component {
+  render() {
+    return (
+      <NEWSTYLE>
+        <div>New Container | Template</div>
+      </NEWSTYLE>
+    );
+  }
+}
