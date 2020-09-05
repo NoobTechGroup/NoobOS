29aa59b9efbfb0f272e5fef899af5b61f53d7ee9
Mauricio Alejandro <mamv.2137@gmail.com>
2020-09-05 17:45:05 -0300

    trys...

--- src/Pages/Desktop/Desktop.jsx
+++ src/Pages/Desktop/Desktop.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+import { cartItemsVar } from "apolloProvider.js";
+
 import "App.css";
 
+const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`;
+
 const Desktop = () => {
+  const { loading, error, data } = useQuery(GET_CART_ITEMS);
+
+  if (!loading) console.log(data);
   return (
     <div className="App-header">
       Desktop...
